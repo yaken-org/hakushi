6a2f00f46a4d90403db02a0e022a0e76933dc525
年輪 <nenrin.yearring@gmail.com>
2024-09-14 17:48:44 +0900

    フロントログ消し忘れ

--- frontend/src/components/auth/signInButton.tsx
+++ frontend/src/components/auth/signInButton.tsx
@@ -5,15 +5,13 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function SignInButton() {
     const nowPath = usePathname();
-    console.log(nowPath);
     const { data: session } = useSession();
     if (session) {
-        console.log(session);
         return (
             <Button onClick={() => signOut()}>SignOut</Button>
         )
     }
     return (
-        <Button onClick={() => signIn("google", { redirectTo: "/"})}>SignIn</Button>
+        <Button onClick={() => signIn("google", { redirectTo: nowPath})}>SignIn</Button>
     )
 }
