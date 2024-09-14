ce0ecf4a43d667e168c7df9e0022d971521f8cde
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-09-14 12:48:01 +0900

    初期DBスキーマを書いてみる

---
+++ db/hakushi.sql
@@ -0,0 +1,58 @@
+CREATE TABLE IF NOT EXSITS `user_account` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `auth_user` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `user_account_id` bigint NOT NULL,
+
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`),
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `post` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+
+    `user_account_id` bigint NOT NULL,
+
+    `title` varchar(255) NOT NULL,
+    `content` text NOT NULL,
+
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `post_item` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+
+    `post_id` bigint NOT NULL,
+    `product_id` bigint NOT NULL,
+
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `product` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+
+    `name` varchar(255) NOT NULL,
+    `link` text NOT NULL,
+
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
