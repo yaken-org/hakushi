756f19ec4bd9bb5f82fc6016a233702be23b9007
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-09-14 16:20:43 +0900

    仮のバックエンドサーバを建てる

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
