823e2240d1318f2ea5d4d254259136d83d36ada9
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-09-15 11:35:14 +0900

    rowからmodelへの変換

--- backend/internal/model/model.go
+++ backend/internal/model/model.go
@@ -5,3 +5,9 @@ import "database/sql"
 type Model interface {
 	FromRow(row *sql.Row) error
 }
+
+func QueryRow[T Model](db *sql.DB, model T, query string, args ...any) (T, error) {
+	row := db.QueryRow(query, args...)
+	err := model.FromRow(row)
+	return model, err
+}
