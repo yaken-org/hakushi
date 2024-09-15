a0dc878b7152a34e5e4009ccc9b557c15adc9f8c
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-09-15 21:28:12 +0900

    スキーマの定義ミスを修正

--- db/migrations/000008_20240915172457_add-display-name-to-user-account-table.up.sql
+++ db/migrations/000008_20240915172457_add-display-name-to-user-account-table.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `user_account`
-ADD COLUMN `display_name` varchar(255) NOT NULL,
-ADD COLUMN `icon_url` text NOT NULL,
-ADD COLUMN `sub` varchar(255) NOT NULL UNIQUE;
+ADD COLUMN `display_name` varchar(255) NOT NULL AFTER `name`,
+ADD COLUMN `icon_url` text NOT NULL AFTER `display_name`,
+ADD COLUMN `sub` varchar(255) NOT NULL UNIQUE AFTER `icon_url`;
